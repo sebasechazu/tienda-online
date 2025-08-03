c41863d2b10336c7f0960a97b190df8a9443685b
Natalia <alamo.natalia@gmail.com>
2025-08-03 22:35:00 +0200

    ajustando estilos en front y back

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -18,11 +18,11 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
     trigger('hoverAnimation', [
       state('initial', style({
         transform: 'scale(1)',
-        boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)'
+        boxShadow: '0 1px 3px 0 rgb(0 0 0 / 0.05), 0 1px 2px -1px rgb(0 0 0 / 0.05)'
       })),
       state('hovered', style({
-        transform: 'scale(1.03)',
-        boxShadow: '0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)'
+        transform: 'scale(1.02)',
+        boxShadow: '0 10px 15px -5px rgb(0 0 0 / 0.1), 0 4px 10px -6px rgb(0 0 0 / 0.1)'
       })),
       transition('initial <=> hovered', [
         animate('200ms ease-in-out')
